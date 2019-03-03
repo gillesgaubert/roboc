87dacc5176a2d93fde2856972c6cac166b0490c3
gillesgaubert <gilles.gaubert@rocketmail.com>
2019-03-03 14:35:42 +0100

    Implementation du deplacement de base

--- labyrinthe.py
+++ labyrinthe.py
@@ -14,56 +14,74 @@ class Labyrinthe:
         index=0
         numLigne=0
         numColonne=0
-        print(chaine)
-        for i in range(len(chaine)-1):
-        	car=chaine[i]
-        	if car=="\n" :
-        		numLigne+=1
-        		numColonne=0
-        	else :
-        		cle=(numColonne,numLigne)
-        		self.grille[cle]=car
-        		print(str(cle[0])+", "+str(cle[1])+" : "+self.grille.get(cle))
-        		if car=='X' :
-        			self.robot['x']=cle[0]
-        			self.robot['y']=cle[1]
-        		numColonne+=1
+        #print(chaine)
+        for i in range(len(chaine)):
+            car=chaine[i]
+            if car=="\n" :
+                numLigne+=1
+                numColonne=0
+            else :
+                cle=(numColonne,numLigne)
+                self.grille[cle]=car
+                #print(str(cle[0])+", "+str(cle[1])+" : "+self.grille.get(cle))
+                #print(type(self.grille.get(cle)))
+                if car=='X' :
+                    self.robot['x']=cle[0]
+                    self.robot['y']=cle[1]
+                    #print("Le robot se trouve colonne="+str(cle[0])+" ligne="+str(cle[1]))
+                numColonne+=1
 
 
 
-    """ grille={(absisse,ordonnee):"elementDeDecor",...}
-        robot=[abs,ordonnee]
+    """ grille={(colonne,ligne):"elementDeDecor",...}
+        robot={'x':valColonne,'y':valLigne}
     """
 
     def __repr__(self):
-    	sortie=""
-    	ligneExiste=True
-    	l=0
-    	c=0
-    	
-    	
+        sortie=""
+        ligneExiste=True
+        l=0
+        c=0
+        
+        while ligneExiste:
+            if not (c,l) in self.grille :
+                #print(str((c,l))+" est non defini, ligne existe pas")
+                ligneExiste=False
+            else :
+                colonneExiste=True
+                while colonneExiste:
+                    if not (c,l) in self.grille :
+                        #print(str((c,l))+" est non defini, colonne existe pas")
+                        sortie+="\n"
+                        colonneExiste=False
+                    else :
+                        #print(str((c,l))+" est defini")
+                        sortie+=self.grille[(c,l)]
+                        c+=1
+                c=0
+                l+=1
+        
+        return sortie
 
-    	"""
-    	while ligneExiste:
-    		if self.grille.get(l,c) is None :
-    			ligneExiste=False
-    		else :
-    			colonneExiste=True
-    			while colonneExiste:
-    				if self.grille.get(l,c) is None :
-    					sortie+="\n"
-    					colonneExiste=False
-    				else :
-    					sortie+=str(self.grille.get(l,c))
-    					c+=1
-    		l+=1
-    	"""
-    	print(self.grille.get((1,1)))
-    	print(self.grille.get((1,2)))
-    	print(self.grille.get((1,3)))
-
-    	#sortie=self.grille.get(1,0)+self.grille.get(1,1)+self.grille.get(1,2)+self.grille.get(1,3)
-
-    	return sortie
-
+    def deplacementRobot(self,commandeDeplacement):
+        lDepl=0
+        cDepl=0
+        print("Dans la fct de deplacement")
+        # ici je prends le premier caractere comme direction
+        dirDeplacement=commandeDeplacement
+        if dirDeplacement.lower()=='n' :
+            lDepl-=1
+        elif dirDeplacement.lower()=='s' :
+            lDepl+=1
+        elif dirDeplacement.lower()=='e' :
+            cDepl+=1
+        elif dirDeplacement.lower()=='o' :
+            cDepl-=1
+        else :
+            print("t'est un couillon")
+        # maintenant on deplace le robot sans controle pour commancer
+        self.grille[(self.robot['x'],self.robot['y'])]=' '
+        self.robot['x']+=cDepl
+        self.robot['y']+=lDepl
+        self.grille[(self.robot['x'],self.robot['y'])]='X'
 
--- roboc.py
+++ roboc.py
@@ -15,20 +15,27 @@ cartes = []
 for nom_fichier in os.listdir("cartes"):
     if nom_fichier.endswith(".txt"):
         chemin = os.path.join("cartes", nom_fichier)
-        nom_carte = nom_fichier[:-3].lower()
+        nom_carte = nom_fichier[:-4].lower()
         with open(chemin, "r") as fichier:
             contenu = fichier.read()
-            print(nom_carte)
-            print(contenu)
+            #print(nom_carte)
+            #print(contenu)
             # Création d'une carte, à compléter
             carte=Carte(nom_carte,contenu)
             #print(carte)
-            print(carte.nom)
-            print(carte.labyrinthe)
+            #print(carte.nom)
+            #print(carte.labyrinthe)
         
-        #cartes.append(carte)
+        cartes.append(carte)
 
-
+print("sur la carte facile : 1")
+print("Maintenant je veux deplacer le robot vers le sud")
+direction='s'
+cacarte=cartes[1]
+print(cacarte)
+lab=cacarte.labyrinthe
+lab.deplacementRobot(direction)
+print(cacarte)
 
 
 
